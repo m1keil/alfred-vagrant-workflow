f3df9b468d611934d16125343414a694cbc21c1c
Michael Sverdlik <michael@gigaspaces.com>
2014-10-24 00:22:23 +0300

    change notification about on-going job
    previously, notification about job that isn't finished would pop up after action select.
    now it won't let you pick an action

--- src/vagrantup.py
+++ src/vagrantup.py
@@ -13,20 +13,20 @@ ICONS_STATES_PATH = os.path.join(Workflow().workflowdir, 'icons', 'states')
 ICONS_ACTION_PATH = os.path.join(Workflow().workflowdir, 'icons', 'actions')
 
 
-def _get_machine_data():
+def get_machine_data():
     vagrant_home = os.environ.get('VAGRANT_HOME', VAGRANT_HOME)
     index_path = os.path.join(vagrant_home, VAGRANT_INDEX)
     with open(index_path) as index:
         data = load(index)
-    _validate_version(data['version'])
+    validate_version(data['version'])
     return data['machines']
 
 
-def _get_state_icon(state, provider):
+def get_state_icon(state, provider):
     """
     Return appropriate icon path for state
     """
-    norm_state = _normalize_state(state)
+    norm_state = normalize_state(state)
     icon = os.path.join(ICONS_STATES_PATH,
                         '{0}.{1}.png'.format(provider, norm_state))
     default = os.path.join(ICONS_STATES_PATH,
@@ -40,7 +40,7 @@ def _get_state_icon(state, provider):
         return None
 
 
-def _get_action_icon(action):
+def get_action_icon(action):
     """
     Return icon path for action
     """
@@ -51,7 +51,7 @@ def _get_action_icon(action):
         return None
 
 
-def _normalize_state(state):
+def normalize_state(state):
     """
     Normalize environment state
     """
@@ -62,7 +62,7 @@ def _normalize_state(state):
     return 'unexpected'
 
 
-def _get_search_key(machine):
+def get_search_key(machine):
     """
     Return search key to be used by Workflow.filter
     """
@@ -73,7 +73,7 @@ def _get_search_key(machine):
     return ' '.join(fields)
 
 
-def _list_machines(machines, wf):
+def list_machines(machines, wf):
     subtitles_dict = {'cmd': 'Run commands on whole environment'}
 
     for mid, meta in machines.iteritems():
@@ -83,17 +83,17 @@ def _list_machines(machines, wf):
                     arg=mid,
                     uid=mid,
                     valid=True,
-                    icon=_get_state_icon(meta['state'], meta['provider']))
+                    icon=get_state_icon(meta['state'], meta['provider']))
 
 
-def _list_actions(eid, wf):
+def list_actions(eid, wf):
     if os.path.isdir(eid):
         def test(info):
             return not info['dir_action']
     else:
-        machine_data = _get_machine_data()
-        state = _normalize_state(machine_data[eid]['state'])
-        norm_state = _normalize_state(state)
+        machine_data = get_machine_data()
+        state = normalize_state(machine_data[eid]['state'])
+        norm_state = normalize_state(state)
 
         def test(info):
             return norm_state not in info['state']
@@ -104,13 +104,13 @@ def _list_actions(eid, wf):
         wf.add_item(title=action,
                     subtitle=prop['desc'],
                     uid=action,
-                    arg='{} {!r}'.format(
+                    arg='{0} {1!r}'.format(
                         eid, ' '.join([action, prop['flags'] or ''])),
-                    icon=_get_action_icon(action),
+                    icon=get_action_icon(action),
                     valid=True)
 
 
-def _validate_version(version):
+def validate_version(version):
     if version != 1:
         raise Exception('Unsupported index version')
 
@@ -143,42 +143,45 @@ def main(wf):
     args = parser.parse_args(wf.args)
 
     if args.list is not None:
-        machine_data = _get_machine_data()
+        machine_data = get_machine_data()
         if args.list:
             machine_data = dict(wf.filter(query=args.list,
                                           items=machine_data.items(),
-                                          key=_get_search_key,
+                                          key=get_search_key,
                                           match_on=MATCH_ALL ^ MATCH_ALLCHARS))
-        _list_machines(machine_data, wf)
+        list_machines(machine_data, wf)
     elif args.set:
-        logger.debug('saving id: {}'.format(args.set))
+        logger.debug('saving id: {0}'.format(args.set))
         wf.cache_data('id', args.set)
         run_alfred(':vagrant-id')
     elif args.setenv:
-        machine_data = _get_machine_data()
+        machine_data = get_machine_data()
         vagrant_dir = machine_data[args.setenv]['vagrantfile_path']
-        logger.debug('saving id: {}'.format(vagrant_dir))
+        logger.debug('saving id: {0}'.format(vagrant_dir))
         wf.cache_data('id', vagrant_dir)
         run_alfred(':vagrant-id')
     elif args.get:
         eid = wf.cached_data('id', max_age=2)
-        logger.debug('retrieved id: {}'.format(eid))
+        vpath = eid
+        if not os.path.isfile(eid):
+            machine_data = get_machine_data()
+            vpath = machine_data[eid]['vagrantfile_path']
+        task_name = 'exec_{0}'.format(hash(vpath))
+        logger.debug('retrieved id: {0}'.format(eid))
         if eid is None:
             raise RuntimeError('No environment id cached')
+        elif is_running(task_name):
+            raise Exception('Task in progress')
         else:
-            _list_actions(eid, wf)
+            list_actions(eid, wf)
     elif args.execute:
-        machine_data = _get_machine_data()
+        machine_data = get_machine_data()
         vpath = args.execute[0]
         if not os.path.isdir(vpath):
             vpath = machine_data[args.execute[0]]['vagrantfile_path']
-
-        task_name = 'exec_{}'.format(hash(vpath))
+        task_name = 'exec_{0}'.format(hash(vpath))
         cmd = ['/usr/bin/python', 'execute.py'] + args.execute
-        if not is_running(task_name):
-            run_in_background(task_name, cmd)
-        else:
-            send_notification('Task in progress. \nAborting')
+        run_in_background(task_name, cmd)
 
     wf.send_feedback()
 
